21338cd12f9c449fdb24267f10a1a84fa9d1f554
hensleyas21 <hensleyas21@gcc.edu>
2025-03-11 16:59:03 -0400

    Update ComputerGuessesPanel.java

--- src/ComputerGuessesPanel.java
+++ src/ComputerGuessesPanel.java
@@ -28,20 +28,51 @@ public class ComputerGuessesPanel extends JPanel {
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
         //not sure if maybe text adjustment needs its own method
-        JLabel guessMessage = new JLabel("I guess ___.");
-        guessMessage.setAlignmentX(Component.LEFT_ALIGNMENT);
-        this.add(guessMessage);
-        guessMessage.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel guessMessage = guessMessage();
 
         this.add(Box.createRigidArea(new Dimension(0, 40)));
 
         //same thing here, maybe consider moving it all to its own method
+        prompt();
+
+        //definitely its own method, especially for the math
+        JButton lowerBtn = lowerButton(guessMessage);
+
+        //same as lower
+        JButton correctBtn = correctButton(cardsPanel, gameFinishedCallback, guessMessage);
+
+        //same as lower and equal
+        JButton higherBtn = higherBtn(guessMessage);
+
+
+        this.addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentShown(java.awt.event.ComponentEvent e) {
+                numGuesses = 0;
+                upperBound = 1000;
+                lowerBound = 1;
+
+                lastGuess = (lowerBound + upperBound + 1) / 2;
+                guessMessage.setText("I guess " + lastGuess + ".");
+            }
+        });
+    }
+
+    private JLabel guessMessage() {
+        JLabel guessMessage = new JLabel("I guess ___.");
+        guessMessage.setAlignmentX(Component.LEFT_ALIGNMENT);
+        this.add(guessMessage);
+        guessMessage.setAlignmentX(Component.CENTER_ALIGNMENT);
+        return guessMessage;
+    }
+
+    private void prompt() {
         JLabel prompt = new JLabel("Your number is...");
         this.add(prompt);
         prompt.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.add(Box.createRigidArea(new Dimension(0,10)));
+    }
 
-        //definitely its own method, especially for the math
+    private JButton lowerButton(JLabel guessMessage) {
         JButton lowerBtn = new JButton("Lower");
         lowerBtn.addActionListener(e -> {
             upperBound = Math.min(upperBound, lastGuess);
@@ -53,8 +84,10 @@ public class ComputerGuessesPanel extends JPanel {
         this.add(lowerBtn);
         lowerBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.add(Box.createRigidArea(new Dimension(0,10)));
+        return lowerBtn;
+    }
 
-        //same as lower
+    private JButton correctButton(JPanel cardsPanel, Consumer<GameResult> gameFinishedCallback, JLabel guessMessage) {
         JButton correctBtn = new JButton("Equal");
         correctBtn.addActionListener(e -> {
             guessMessage.setText("I guess ___.");
@@ -68,9 +101,11 @@ public class ComputerGuessesPanel extends JPanel {
         });
         this.add(correctBtn);
         correctBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-        this.add(Box.createRigidArea(new Dimension(0,10)));
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
+        return correctBtn;
+    }
 
-        //same as lower and equal
+    private JButton higherBtn(JLabel guessMessage) {
         JButton higherBtn = new JButton("Higher");
         higherBtn.addActionListener(e -> {
             lowerBound = Math.max(lowerBound, lastGuess + 1);
@@ -81,19 +116,6 @@ public class ComputerGuessesPanel extends JPanel {
         });
         this.add(higherBtn);
         higherBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-
-        //i dont think we should touch this...
-        this.addComponentListener(new java.awt.event.ComponentAdapter() {
-            public void componentShown(java.awt.event.ComponentEvent e) {
-                numGuesses = 0;
-                upperBound = 1000;
-                lowerBound = 1;
-
-                lastGuess = (lowerBound + upperBound + 1) / 2;
-                guessMessage.setText("I guess " + lastGuess + ".");
-            }
-        });
+        return higherBtn;
     }
-
 }
