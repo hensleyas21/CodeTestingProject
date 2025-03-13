0a0a932be245d0065cf7f73d1064fc1992dcc0de
hensleyas21 <hensleyas21@gcc.edu>
2025-03-13 10:06:08 -0400

    TestGameResult finished

--- test/TestGameResult.java
+++ test/TestGameResult.java
@@ -3,5 +3,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestGameResult {
     @Test
-    public void Test
+    public void testGameResultConstructor() {
+        GameResult gr1 = new GameResult(true, 10, 5);
+        assertTrue(gr1.humanWasPlaying);
+        assertEquals(10, gr1.correctValue);
+        assertEquals(5, gr1.numGuesses);
+
+        GameResult gr2 = new GameResult(false, 7, 3);
+        assertFalse(gr2.humanWasPlaying);
+        assertEquals(7, gr2.correctValue);
+        assertEquals(3, gr2.numGuesses);
+    }
 }
